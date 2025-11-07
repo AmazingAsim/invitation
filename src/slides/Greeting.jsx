f535bf7aa1fc82b48e577267c3c7d3b53d135672
AmazingAsim <101821929+AmazingAsim@users.noreply.github.com>
2025-11-07 20:49:15 +0530

    reduced tiimeing

--- src/slides/Greeting.jsx
+++ src/slides/Greeting.jsx
@@ -10,7 +10,7 @@ export default function Greeting() {
     useEffect(function(){
         setTimeout(function(){
             navigate('/venue')
-        },8000)
+        },3000)
     },[])
 
     return (
